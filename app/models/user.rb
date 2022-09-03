bdf880a2b70a804451a5ea8ad790e43297dfc570
hans bousquet lafond <hans.bl@icloud.com>
2022-09-03 17:01:05 +0100

    users signup fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :kits
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :company_name, presence: true
