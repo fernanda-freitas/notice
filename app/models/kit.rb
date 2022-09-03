9be308876d167b8b3e13657f167daec69a1f5443
hans bousquet lafond <hans.bl@icloud.com>
2022-09-03 11:05:25 +0100

    validations OK for users kits and steps

--- app/models/kit.rb
+++ app/models/kit.rb
@@ -1,4 +1,7 @@
 class Kit < ApplicationRecord
   belongs_to :user
   has_many :steps
+
+  validates :title, presence: true, length: { in: 8..32 }
+  validates :description, presence: true, length: { in: 8..64 }
 end
--- app/models/step.rb
+++ app/models/step.rb
@@ -1,3 +1,6 @@
 class Step < ApplicationRecord
   belongs_to :kit
+
+  validates :content, presence: true, length: { in: 32..650 }
+  validates :todo, presence: true, length: { in: 6..64 }
 end
