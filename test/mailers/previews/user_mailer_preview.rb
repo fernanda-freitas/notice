60f40adb53bfc2040481fecdcb860d1fdbd8c436
Hila Pickman <pickman.hila@gmail.com>
2022-09-18 10:53:02 +0200

    view fixed

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -2,9 +2,9 @@
 class UserMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/user_mailer/welcome
-  def welcome
+  def sharekit
     user = User.first
     # This is how you pass value to params[:user] inside mailer definition!
-    UserMailer.with(user: user).welcome
+    UserMailer.with(user: user).sharekit
   end
 end
