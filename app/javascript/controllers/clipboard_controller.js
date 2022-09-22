229bb014484d2668555d521489753226810d910b
fernanda freitas <idea.fernanda@gmail.com>
2022-09-22 18:40:33 +0100

    stimulus controllers

--- app/javascript/controllers/clipboard_controller.js
+++ app/javascript/controllers/clipboard_controller.js
@@ -3,7 +3,6 @@ import { Controller } from "stimulus"
 export default class extends Controller {
   static targets = [ "input", "copy", "check", "button" ]
 
-
   copy() {
     const urlToShare = this.inputTarget.value;
     navigator.clipboard.writeText(urlToShare);
